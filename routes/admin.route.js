77eb358f6022e8858042392b471a67a3ac62ff5f
Damiano <damiano.mondaini@gmail.com>
2019-05-17 11:22:07 +0100

    Uncommented user on dashboard

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -4,11 +4,11 @@ let user_controller = require('../controllers/user.controller')
 let todo_controller = require('../controllers/todo.controller')
 
 router.get('/', user_controller.user_count, todo_controller.todo_all, (req, res) => {
-    res.render('admin/dashboard', { data: res.locals, board: 'main'/*, user: req.user.username */})
+    res.render('admin/dashboard', { data: res.locals, board: 'main', user: req.user.username })
 })
 
 router.get('/users', user_controller.user_all, (req, res) => {
-    res.render('admin/dashboard', { data: res.locals, board: 'users', /*user: req.user.username*/ })
+    res.render('admin/dashboard', { data: res.locals, board: 'users', user: req.user.username })
 })
 
 router.post('/todo-add', todo_controller.todo_create)
